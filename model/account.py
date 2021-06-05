7c4d5dfc7d3f3d669dee546a223d78782da96948
piyo02 <alzidni2000@gmail.com>
2021-06-06 05:24:14 +0800

    mengubah menjadi berdasarkan kota

--- model/account.py
+++ model/account.py
@@ -10,58 +10,62 @@ class ReportAccountReceivableAge(models.TransientModel):
 
     age = fields.Integer(string='Umur Piutang', required=True)
     customers = fields.Many2many('res.partner', string='Pelanggan', required=False)
+    city_ids = fields.Many2many("vit.kota", string='Kota', required=False)
 
     @api.multi
     def print_report_account_receivable_age(self):
         groupby_dict = {}
 
-        if len(self.customers) == 0:
-            self.customers = self.env['res.partner'].search([
-                ('customer', '=', True)
-            ],
-            order='name asc')
+        if len(self.city_ids) == 0:
+            self.city_ids = self.env['vit.kota'].search([])
 
-        today = date.today()
-        date_invoice = ( today + timedelta( days=(self.age*-1) ) ).strftime("%Y-%m-%d")
-        customer_detail = []
-        for customer in self.customers:
-            invoices = self.env['account.invoice'].search([
-                ('date_invoice', '>=', date_invoice),
-                ('partner_id', '=', customer.id),
-                ('state', 'in', ['draft', 'open'])
-            ])
-            customer_temp = []
-            customer_invoices = []
-            customer_temp.append(customer.credit_limit)
-            for invoice in invoices:
-                invoice_date = datetime.strptime(invoice.date_invoice, '%Y-%m-%d').date()
-                invoice_age = today - invoice_date
+        for city in self.city_ids:
+            customers = self.env['res.partner'].search([('kota_id.name', '=', city.name)])
 
-                customer_invoice = []
-                customer_invoice.append(invoice.number)         #0
-                customer_invoice.append(invoice.payment_term_id.name)#1
-                customer_invoice.append(invoice.date_invoice)   #2
-                customer_invoice.append(invoice.user_id.name)   #3
-                customer_invoice.append(invoice.date_due)       #4
-                customer_invoice.append(invoice.amount_total)   #5
-                customer_invoice.append(invoice.residual)       #6
-                customer_invoice.append(invoice_age.days)       #7
-                
-                bg_number = '-'
-                giros = self.env['vit.giro'].search([
-                    ('giro_invoice_ids.invoice_id', '=', invoice.id)
+            today = date.today()
+            date_invoice = ( today + timedelta( days=(self.age*-1) ) ).strftime("%Y-%m-%d")
+            customer_detail = []
+            for customer in customers:
+                invoices = self.env['account.invoice'].search([
+                    ('date_invoice', '>=', date_invoice),
+                    ('partner_id', '=', customer.id),
+                    ('state', 'in', ['draft', 'open'])
                 ])
-                if giros:
-                    for giro in giros:
-                        bg_number = bg_number + ' ' + giro.name
-                customer_invoice.append(bg_number)              #8
-                
-                customer_invoices.append(customer_invoice)      
-            
-            customer_temp.append(customer_invoices)
+                customer_temp = []
+                customer_invoices = []
+                customer_temp.append(customer.display_name) #0
+                customer_temp.append(customer.credit_limit) #1
+                for invoice in invoices:
+                    invoice_date = datetime.strptime(invoice.date_invoice, '%Y-%m-%d').date()
+                    invoice_age = today - invoice_date
 
-            if len(customer_invoices) > 0:
-                groupby_dict[customer.display_name] = [customer_temp]
+                    customer_invoice = []
+                    customer_invoice.append(invoice.number)         #0
+                    customer_invoice.append(invoice.payment_term_id.name)#1
+                    customer_invoice.append(invoice.date_invoice)   #2
+                    customer_invoice.append(invoice.user_id.name)   #3
+                    customer_invoice.append(invoice.date_due)       #4
+                    customer_invoice.append(invoice.amount_total)   #5
+                    customer_invoice.append(invoice.residual)       #6
+                    customer_invoice.append(invoice_age.days)       #7
+                    
+                    bg_number = '-'
+                    giros = self.env['vit.giro'].search([
+                        ('giro_invoice_ids.invoice_id', '=', invoice.id)
+                    ])
+                    if giros:
+                        for giro in giros:
+                            bg_number = bg_number + ' ' + giro.name
+                    customer_invoice.append(bg_number)              #8
+                    
+                    customer_invoices.append(customer_invoice)      
+                
+                customer_temp.append(customer_invoices) #2
+                if len(customer_invoices) > 0:
+                    customer_detail.append(customer_temp)
+
+            if len(customer_detail) > 0:
+                groupby_dict[city.name] = customer_detail
         
         datas = {
             'ids': self.ids,
